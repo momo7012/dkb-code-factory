d85b781730afb0a80190f80eefef60ebcf07f795
Mohammadreza Momeni <mohamad7012@gmail.com>
2022-05-17 17:10:00 +0430

    Developing two test fail scenario for hashUtil methods.

--- src/test/kotlin/com/dkbcodefactory/shorturl/util/HashUtilTest.kt
+++ src/test/kotlin/com/dkbcodefactory/shorturl/util/HashUtilTest.kt
@@ -16,4 +16,16 @@ internal class HashUtilTest {
         val base64 = HashUtil.base64("https://www.dkbcodefactory.com/#/")
         assertEquals(base64, "aHR0cHM6Ly93d3cuZGtiY29kZWZhY3RvcnkuY29tLyMv")
     }
+
+    @Test
+    fun test_md5_fail_method() {
+        val md5 = HashUtil.md5("https://www.dkbcodefactory.com/#/")
+        assertNotEquals(md5, "671a2aa3cb846f06d356b188e4f0")
+    }
+
+    @Test
+    fun test_bas264_fail_method() {
+        val base64 = HashUtil.base64("https://www.dkbcodefactory.com/#/")
+        assertNotEquals(base64, "RvcnkuY29tLyMvaHR0cHM6Ly93d3cuZGtiY29kZWZhY3")
+    }
 }
