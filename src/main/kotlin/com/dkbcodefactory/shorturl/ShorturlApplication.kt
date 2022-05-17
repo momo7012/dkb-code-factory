5cd3e87cfbc0840e73a4e875bdb78563d61a32c2
Mohammadreza Momeni <mohamad7012@gmail.com>
2022-05-17 19:44:09 +0430

    enabling jpa repositories.

--- src/main/kotlin/com/dkbcodefactory/shorturl/ShorturlApplication.kt
+++ src/main/kotlin/com/dkbcodefactory/shorturl/ShorturlApplication.kt
@@ -4,8 +4,12 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition
 import io.swagger.v3.oas.annotations.info.Info
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 
 @SpringBootApplication
+@EnableJpaAuditing
+@EnableJpaRepositories
 @OpenAPIDefinition(
     info = Info(
         title = "URL shortener",
