59e4b7736abdbb37e8e581c130d0f8040b3a2acb
Mohammadreza Momeni <mohamad7012@gmail.com>
2022-05-17 19:39:34 +0430

    Implementing Url Rest Controller

---
+++ src/main/kotlin/com/dkbcodefactory/shorturl/controller/impl/UrlRestControllerImpl.kt
@@ -0,0 +1,27 @@
+package com.dkbcodefactory.shorturl.controller.impl
+
+import com.dkbcodefactory.shorturl.controller.UrlRestController
+import com.dkbcodefactory.shorturl.dto.UrlRequest
+import com.dkbcodefactory.shorturl.dto.UrlResponse
+import com.dkbcodefactory.shorturl.service.UrlService
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import javax.websocket.server.PathParam
+
+@RestController
+@RequestMapping("/url")
+class UrlRestControllerImpl(private val urlService: UrlService ) : UrlRestController {
+
+    @PostMapping("/short")
+    override fun getShortUrl(@RequestBody request : UrlRequest) : UrlResponse {
+        return urlService.checkingUrl(request)
+    }
+
+    @GetMapping("/original/{shortUrl}")
+    override fun getOriginalUrl(@PathParam("shortUrl") shortUrl: String): UrlResponse {
+        return urlService.findOriginalUrl(shortUrl);
+    }
+}
