6172ea0627e70fbcb1bfba3614c3aa5fd75e61fa
Mohammadreza Momeni <mohamad7012@gmail.com>
2022-05-17 17:03:53 +0430

    test base64 method.

--- src/test/kotlin/com/dkbcodefactory/shorturl/util/HashUtilTest.kt
+++ src/test/kotlin/com/dkbcodefactory/shorturl/util/HashUtilTest.kt
@@ -10,4 +10,10 @@ internal class HashUtilTest {
         val md5 = HashUtil.md5("https://www.dkbcodefactory.com/#/")
         assertEquals(md5, "846f06d356b188e4f05094671a2aa3cb")
     }
+
+    @Test
+    fun test_bas264_method() {
+        val base64 = HashUtil.base64("https://www.dkbcodefactory.com/#/")
+        assertEquals(base64, "aHR0cHM6Ly93d3cuZGtiY29kZWZhY3RvcnkuY29tLyMv")
+    }
 }
