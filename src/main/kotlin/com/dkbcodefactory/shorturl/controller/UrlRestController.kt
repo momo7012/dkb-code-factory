e86d4d41296f5d6b1d5aa9c075e12ea033ed6ac2
Mohammadreza Momeni <mohamad7012@gmail.com>
2022-05-18 12:52:27 +0430

    fix bug - adding path variable

--- src/main/kotlin/com/dkbcodefactory/shorturl/controller/UrlRestController.kt
+++ src/main/kotlin/com/dkbcodefactory/shorturl/controller/UrlRestController.kt
@@ -7,6 +7,7 @@ import io.swagger.v3.oas.annotations.media.Content
 import io.swagger.v3.oas.annotations.media.Schema
 import io.swagger.v3.oas.annotations.responses.ApiResponse
 import io.swagger.v3.oas.annotations.responses.ApiResponses
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestBody
 import javax.websocket.server.PathParam
 
@@ -45,5 +46,5 @@ interface UrlRestController {
             )
         ]
     )
-    fun getOriginalUrl(@PathParam("shortUrl") shortUrl: String) : UrlResponse
+    fun getOriginalUrl(@PathVariable("shortUrl") shortUrl: String): UrlResponse
 }
--- src/main/kotlin/com/dkbcodefactory/shorturl/controller/impl/UrlRestControllerImpl.kt
+++ src/main/kotlin/com/dkbcodefactory/shorturl/controller/impl/UrlRestControllerImpl.kt
@@ -5,6 +5,7 @@ import com.dkbcodefactory.shorturl.dto.UrlRequest
 import com.dkbcodefactory.shorturl.dto.UrlResponse
 import com.dkbcodefactory.shorturl.service.UrlService
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -21,7 +22,7 @@ class UrlRestControllerImpl(private val urlService: UrlService ) : UrlRestContro
     }
 
     @GetMapping("/{shortUrl}")
-    override fun getOriginalUrl(@PathParam("shortUrl") shortUrl: String): UrlResponse {
+    override fun getOriginalUrl(@PathVariable("shortUrl") shortUrl: String): UrlResponse {
         return urlService.findOriginalUrl(shortUrl);
     }
 }
